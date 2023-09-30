3863ad726c3d545307eeb7503b974ba8dad3558a
aliaksandr <bernatik@tut.by>
2023-09-30 16:57:02 +0200

    add mutex to AssetsTextRepository to manage access to cached text

--- app/src/main/java/com/alexbernat/wordscounter/data/AssetsTextRepository.kt
+++ app/src/main/java/com/alexbernat/wordscounter/data/AssetsTextRepository.kt
@@ -2,18 +2,24 @@ package com.alexbernat.wordscounter.data
 
 import android.content.res.AssetManager
 import com.alexbernat.wordscounter.domain.TextRepository
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
 
 class AssetsTextRepository(
     private val assets: AssetManager
 ) : TextRepository {
 
+    private val mutex = Mutex()
+
     private var cachedString: String? = null
 
     override suspend fun loadText(): String {
-        return cachedString ?: assets.open(FILE_NAME).bufferedReader().use {
-            it.readText()
-        }.also { resultString ->
-            cachedString = resultString
+        return cachedString ?: mutex.withLock {
+            cachedString ?: assets.open(FILE_NAME).bufferedReader().use {
+                it.readText()
+            }.also { resultString ->
+                cachedString = resultString
+            }
         }
     }
 
